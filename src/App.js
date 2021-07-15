4b526b6fa66c3f460cc13584db127bffab6b7a56
Stefan Lukic <33794710+Laemonz@users.noreply.github.com>
2021-07-14 20:43:33 -0400

    added 404 page

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Browse from './components/Browse/Browse'
 import Search from './components/Search/Search'
 import List from './components/List/List'
 import Profile from './components/Profile/Profile';
+import AddUser from "./components/UserLogin/AddUser";
 import PageNotFound from './components/PageNotFound/PageNotFound'
 
 import { Auth0Provider } from "@auth0/auth0-react";
 
 class App extends Component {
   render() {
+    console.log('uri:'+window.location.origin+'/loggedIn');
     return (
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
@@ -23,9 +25,10 @@ class App extends Component {
       <Navigator/>
         <Switch>
             <Route path="/" exact component={Browse} />
-            <Route path="/search" exact component={Search} />
-            <Route path="/list" exact component={List} />
-            <Route path="/profile" exact component={Profile} />
+            <Route path="/search" component={Search} />
+            <Route path="/list" component={List} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/loggedIn" component={AddUser} />
             <Route path='*' component={PageNotFound} />
         </Switch>
       </Router>
