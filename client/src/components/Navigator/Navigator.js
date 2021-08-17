4b0dc0963fbc8c1d9784dba4547969b90111a097
Stefan Lukic <33794710+Laemonz@users.noreply.github.com>
2021-08-17 12:31:37 -0400

    replace text with home icon

--- client/src/components/Navigator/Navigator.js
+++ client/src/components/Navigator/Navigator.js
@@ -4,18 +4,28 @@ import { Nav, Navbar, Container, Image } from 'react-bootstrap';
 import { BrowserRouter as Route, Link } from "react-router-dom";
 import UserLogin from '../UserLogin//UserLogin'
 import githubIcon from '../../images/GitHub-Mark-32px.png'
+import homeIcon from '../../images/home.png'
+
 
 class Navigator extends Component{
     render(){
         return(
                 <Navbar bg="light" expand="md" collapseOnSelect sticky='top'>
                     <Container>
-                    <Navbar.Brand as={Link} to="/">Movies-App</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/">
+                        <img
+                            alt="home"
+                            src={homeIcon}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}        
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link eventKey="1" as={Link} to="/">Popular Movies</Nav.Link>
-                            <Nav.Link eventKey="2" as={Link} to="/search">Search Movie</Nav.Link>
+                            <Nav.Link eventKey="2" as={Link} to="/search">Search Movies</Nav.Link>
                             <Nav.Link eventKey="3" as={Link} to="/list">Favourites</Nav.Link>
                         </Nav>
                         <Nav className="mr-auto">
